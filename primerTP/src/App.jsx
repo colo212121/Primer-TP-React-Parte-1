d797ee099a63214ea9171640dcac607df0c469ff
ELIAS  BRODSKY <48387263@ort>
2025-04-16 09:17:28 -0300

    terminado

---
+++ primerTP/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+import Formulario from './components/Formulario.jsx'
+import ListadoCitas from './components/ListadoCitas.jsx'
+import './App.css'
+
+function App() {
+  const [citas, setCitas] = useState([])
+
+  function agregarCita(cita) {
+    setCitas([...citas, cita])
+  }
+
+  function eliminarCita(id) {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id)
+    setCitas(nuevasCitas)
+  }
+
+  return (
+    <div className="contenedor">
+      <Header />
+      <Formulario agregarCita={agregarCita} />
+      <ListadoCitas citas={citas} eliminarCita={eliminarCita} />
+    </div>
+  )
+}
+
+export default App
